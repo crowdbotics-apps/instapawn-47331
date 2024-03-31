42327007acdb1071c656b14d9aaa1d1cd65fd530
Crowdbotics <team@crowdbotics.com>
2024-03-31 05:16:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "instapawn-47331",
+  url: "https://instapawn-47331.botics.co",
+  api: "https://instapawn-47331.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
